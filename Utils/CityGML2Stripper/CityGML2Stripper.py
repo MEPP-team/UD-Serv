65a1b965230df0596656449fa1ccfd54ba60295b
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2021-12-13 16:27:56 +0100

    add remove empty buildings funcionality to CityGML2stripper

--- Utils/CityGML2Stripper/CityGML2Stripper.py
+++ Utils/CityGML2Stripper/CityGML2Stripper.py
@@ -9,22 +9,31 @@ def ParseCommandLine():
             file. It removes appearences and generic attributes.'''
     parser = argparse.ArgumentParser(description=descr)
     parser.add_argument('--input',
-                        nargs='+',
+                        required=True,
                         type=str,
-                        help='CityGML input file')
+                        help='CityGML input file.')
     parser.add_argument('--output',
-                        nargs='+',
                         default='output.gml',
                         type=str,
                         help='Resulting file.')
     parser.add_argument('--remove-building-parts',
                         action='store_true',
-                        help='If set, remove constistsOfBuildingPart and BuildingPart elements.')
+                        help='Move constistsOfBuildingPart and BuildingPart content to parent Building.')
+    parser.add_argument('--remove-empty-buildings',
+                        action='store_true',
+                        help='Remove Buildings which do not have any solid or surface geometry. This step will take place after --remove-building-parts if set.')
     return parser.parse_args()
 
 if __name__ == '__main__':
     cli_args = ParseCommandLine()
-    filename = cli_args.input[0]
+    filename = cli_args.input
+    geometry_list = [
+        'boundedBy',
+        'lod1Solid',
+        'lod2Solid',
+        'lod3Solid',
+        'lod4Solid'
+    ]
 
     # parse file
     parser = ET.XMLParser(remove_comments=True)
@@ -34,10 +43,10 @@ if __name__ == '__main__':
     # in submodule lxml.etree, function strip_elements
     # Remove all elements in the namespace app
     ET.strip_elements(parsed_file,
-                      '{' + parsed_file.getroot().nsmap['app'] + '}' + '*')
+                      '{' + parsed_file.getroot().nsmap['app'] + '}*')
     # Remove all generic elements
     ET.strip_elements(parsed_file,
-                      '{' + parsed_file.getroot().nsmap['gen'] + '}' + '*')
+                      '{' + parsed_file.getroot().nsmap['gen'] + '}*')
     # If remove-building-parts, get all BuildingParts and copy their content to the parent building
     if cli_args.remove_building_parts:
         for building_part in parsed_file.getroot().findall('.//{' + parsed_file.getroot().nsmap['bldg'] + '}BuildingPart'):
@@ -50,10 +59,36 @@ if __name__ == '__main__':
                 building.append(child)
         # Remove all bldg:constistsOfBuildingPart* elements
         ET.strip_elements(parsed_file,
-                            '{' + parsed_file.getroot().nsmap['bldg'] + '}constistsOfBuildingPart' + '*')
+                            '{' + parsed_file.getroot().nsmap['bldg'] + '}consistsOfBuildingPart' + '*')
+    # If remove-empty-buildings, get all Buildings and remove them if they do not contain solids or boundary surfaces
+    if cli_args.remove_empty_buildings:
+        # map the Building namespace to the geometry list
+        addBldgNamespace = lambda tag : '{' + parsed_file.getroot().nsmap['bldg'] + '}' + tag
+        geometry_list = list(map(addBldgNamespace, geometry_list))
+        for building in parsed_file.getroot().findall('.//{' + parsed_file.getroot().nsmap['bldg'] + '}Building'):
+            has_geometry = False
+            # if any Building children tags are in the geometry list, the Building is not considered empty
+            for child in building:
+                if child.tag in geometry_list:
+                    has_geometry = True
+                    break
+            # also check building parts if they have not already been removed
+            if not cli_args.remove_building_parts:
+                for building_part in building.findall('.//{' + parsed_file.getroot().nsmap['bldg'] + '}consistsOfBuildingPart'):
+                    for child in building_part[0]:
+                        if child.tag in geometry_list:
+                            has_geometry = True
+                            break
+                    if has_geometry:
+                        break
+            # if the building is considered empty, remove the cityObjectMember element that contains it
+            if not has_geometry:
+                city_object_member = building.getparent()
+                parsed_file.getroot().remove(city_object_member)
                 
+
     # Concerning the xml_declaration and encoding tags, refer e.g. to 
     # https://stackoverflow.com/questions/12966488/preserving-original-doctype-and-declaration-of-an-lxml-etree-parsed-xml
-    parsed_file.write(cli_args.output[0],
+    parsed_file.write(cli_args.output,
                       xml_declaration=True,
                       encoding=parsed_file.docinfo.encoding)
